e500c4bc9290d7796604a8085398cd1f8a836e3c
William Robersone <william.robersone@gmail.com>
2020-09-05 12:25:06 -0700

    first commit

---
+++ Instagram/Controllers/CoreTabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by William Robersone on 9/5/20.
+//  Copyright © 2020 William Robersone. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Instagram/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by William Robersone on 9/5/20.
+//  Copyright © 2020 William Robersone. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
